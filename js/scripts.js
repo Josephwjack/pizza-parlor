d01478af62b901aebae5fce5185885dab43e0186
Josephwjack <joseph.w.jack@gmail.com>
2022-06-17 14:13:38 -0700

    WIP ; studying about checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business logic-----
-function Pizza(toppings, size) {
-  this.toppings = toppings; 
+function Pizza(topping, size) {
+  this.toppings = topping; 
   this.size = size;
 }
 Pizza.prototype.pizzaSize = function () {
@@ -9,22 +9,28 @@ Pizza.prototype.pizzaSize = function () {
     this.price -= 3;
   } else if (this.size === "large") {
     this.price += 3;  
-  } else {
-    price = 13;
-  } return this.price;
+  }
 }
 
-Pizza.prototype.toppingsCost = function() {
-  let totalPrice = this.price + toppings;
-  i
-  
-}
+
+// Pizza.prototype.toppingsCost = function() {
+//   let totalPrice = this.price + this.topping;
+//   if (this.topping <= 1) {
+//     totalPrice += 1;
+//   } else if (this.topping <= 2) {
+//     totalPrice += 2;
+//   } else if (this.topping <= 3) {
+//     totalPrice += 3;
+//   } else if (this.topping <=4) {
+//     totalPrice += 4;
+//   } else if (this.topping <= 5) { 
+//    totalPrice += 5;
+//   } 
+//   return totalPrice;
+
 
 
 // UI logic-----
-
-
-let topPrice = 0;
 
 
 
