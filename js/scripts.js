b94f421101d1605f159b8498e02d597ddb6dab52
Josephwjack <joseph.w.jack@gmail.com>
2022-06-26 22:03:40 -0700

    add functionality still need css

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 function Pizza(size, toppings) {
   this.size = size;
-  this.toppings = toppings; 
+  this.toppings = toppings;
 }
 
 
@@ -13,7 +13,7 @@ Pizza.prototype.pizzaPrice = function () {
   } else if (this.size === "medium") {
     price = 13;
   } else if (this.size === "large") {
-    price = 16;  
+    price = 16;
   }
   // add topings price to total
   if (this.toppings) {
@@ -26,28 +26,24 @@ Pizza.prototype.pizzaPrice = function () {
   return price;
 }
 
-    
-  
-  
-  // UI logic-----
- 
-//   let toppingHolder = [];
-//   let topPrice = 0;
-//   let tops = [];
-// }
-$(document).ready(function() {
-  $('#pizza-build').submit(function(event) {
+
+
+
+// UI logic-----
+
+
+$(document).ready(function () {
+  $('#pizza-build').submit(function (event) {
     event.preventDefault();
     let sizeElem = document.getElementById('size');
     const sizeInput = sizeElem.options[sizeElem.selectedIndex].value;
     let topingsInput = $('input[type="checkbox"]:checked').map(function () {
       return $(this).val();
     });
-    // const sizeInput = $("input[name='size']:selectedIndex").val();
     let pizza = new Pizza(sizeInput, topingsInput);
     const pizzaPrice = pizza.pizzaPrice()
-    console.log(`submit-btn pressed, price = ${pizzaPrice}`);
+    // console.log(`submit-btn pressed, price = ${pizzaPrice}`);
+    $("#results").html(`$${pizzaPrice}`);
+  })
 
-  }) 
-  
 })
